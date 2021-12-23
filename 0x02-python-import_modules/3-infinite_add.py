eef55c04d65322a488db122d7a7ea5bd3c315223
rafed01 <rafedriahi.rr@gmail.com>
2021-12-23 23:01:03 +0100

    0x01

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    x = 0
+    if len(argv) == 1:
+        print("0")
+    else:
+        for i in range(1, len(argv)):
+            x += int(argv[i])
+        print(x)
